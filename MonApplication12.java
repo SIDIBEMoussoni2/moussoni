91c14ac32d7b10c1d1cade3e8a5e6f0cb075c4a8
SIDIBEMoussoni2 <97638336+SIDIBEMoussoni2@users.noreply.github.com>
2022-01-18 11:43:19 +0100

    Add files via upload

---
+++ MonApplication12.java
@@ -0,0 +1,21 @@
+public class MonApplication12 {
+
+	public static void main (String arg[]) {
+		System.out.println("demo condition");
+		
+	int i = 10;
+	
+	if (i < 0)
+	{
+			System.out.println ("le nombre est negatif !");
+	}
+	else
+	{
+		if (1 == 0)
+			System.out.println ("le nombre est nul !");
+			
+		else
+			System.out.println ("le nombre est positif !");
+	}
+   }
+  }
