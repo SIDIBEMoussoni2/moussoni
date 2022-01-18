f78733eea9ade07ec96d286e5c8d3423fc7cfb5a
SIDIBEMoussoni2 <97638336+SIDIBEMoussoni2@users.noreply.github.com>
2022-01-18 11:34:32 +0100

    Add files via upload

---
+++ bonjour.java
@@ -0,0 +1,11 @@
+	/*bonjour.java : ce programme affiche un message de bien venue */
+
+	class bonjour {										//declaration classe
+	
+		public static void main (String arg[]) { // methode main()
+			System.out.println ("bonjour");		 // instruction
+		    }
+		
+	               }	
+			
+	
