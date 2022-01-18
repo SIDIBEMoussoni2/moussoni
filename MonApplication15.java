8e79fe6df5c428dedb205524947c7eb1502f78da
SIDIBEMoussoni2 <97638336+SIDIBEMoussoni2@users.noreply.github.com>
2022-01-18 11:48:20 +0100

    Add files via upload

---
+++ MonApplication15.java
@@ -0,0 +1,13 @@
+public class MonApplication15 {
+
+	public static void main (String arg[]) {
+		System.out.println ("demo condition");
+		
+		int i = 10;
+		
+		if (i < 0) 
+			System.out.println("le monde est negatif");
+		else
+			System.out.println("le monde est positif");
+		}
+}
