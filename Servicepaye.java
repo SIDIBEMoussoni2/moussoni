656fc59c5a2b36a29a10d125d22c8d5c32cb3da4
SIDIBEMoussoni2 <97638336+SIDIBEMoussoni2@users.noreply.github.com>
2022-01-18 12:32:21 +0100

    Add files via upload

---
+++ Servicepaye.java
@@ -0,0 +1,9 @@
+public class ServicePaye {
+
+	public int calculerSalaire (String employe) {
+	System.out.println ("calcul salaire de "+ employe) ;
+		int salaireFixe = 250000 ;
+		return salaireFixe;
+	}
+	
+}	
