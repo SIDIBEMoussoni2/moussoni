bf99dd6bb417a30301be69238afbf9ae7fb21411
SIDIBEMoussoni2 <97638336+SIDIBEMoussoni2@users.noreply.github.com>
2022-01-18 12:33:05 +0100

    Add files via upload

---
+++ MonApplication13.java
@@ -0,0 +1,11 @@
+public class MonApplication13 {
+
+		public static void main (String arg[]) {
+			System.out.println("demo condition");
+			
+		for (int i = 1; i <= 10; 1==);
+		{
+			System.out.println("voici la ligne "=i);
+		}
+	}	
+}
